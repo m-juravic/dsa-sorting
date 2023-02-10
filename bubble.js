7a1aee39957dc17d6ad5b53a8d4d1a7f5389f2ad
Maria Juravic <mariajuravic@yahoo.com>
2023-02-10 11:45:04 -0800

    through bubble sort

--- bubble.js
+++ bubble.js
@@ -1,3 +1,15 @@
-function bubbleSort() {}
+function bubbleSort(values) {
+
+  if (values.length <= 1) return values;
+
+  for (let i = values.length - 1; i >= 0; i--) {
+    for (let j = 0; j < i; j++ ) {
+      if (values[j] > values[j + 1]) {
+        [values[j], values[j + 1]] = [values[j + 1], values[j] ]
+      }
+    }
+  }
+  return values;
+}
 
 module.exports = bubbleSort;
